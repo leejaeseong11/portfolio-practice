627b2a733571a6990dbe8f9ccd990b70b35c4b10
leejaeseong11 <easeon78@gmail.com>
2022-03-11 00:21:33 +0900

    Connect firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
-// Firebase 설정을 위한 import
-// import { firebase } from 'https://www.gstatic.com/firebasejs/4.11.0/firebase.js';
-
-// // Firebase app 설정
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBwzEE5THOJ6RdHkxu-82_8Eg4xqcZEYFE',
-//   authDomain: 'hyangham-portfolio.firebaseapp.com',
-//   databaseURL: 'https://hyangham-portfolio-default-rtdb.firebaseio.com',
-//   projectId: 'hyangham-portfolio',
-//   storageBucket: 'hyangham-portfolio.appspot.com',
-//   messagingSenderId: '810886149707',
-//   appId: '1:810886149707:web:ce1aad1dd89163bab17fce',
-// };
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyBwzEE5THOJ6RdHkxu-82_8Eg4xqcZEYFE',
+  authDomain: 'hyangham-portfolio.firebaseapp.com',
+  databaseURL: 'https://hyangham-portfolio-default-rtdb.firebaseio.com',
+  projectId: 'hyangham-portfolio',
+  storageBucket: 'hyangham-portfolio.appspot.com',
+  messagingSenderId: '810886149707',
+  appId: '1:810886149707:web:ce1aad1dd89163bab17fce',
+};
+firebase.initializeApp(config);
 
 // // Firebase 초기화
 // firebase.initializeApp(firebaseConfig);
 var diaryTitleField = document.getElementById('diary-form-title');
-var diaryTitleResult = document.getElementById('diary-title-result');
+var diaryContentsField = document.getElementById('diary-form-contents');
+var diaryTitleResultField = document.getElementById('diary-result-title');
+var diaryContentsResultField = document.getElementById('diary-result-contents');
+var diaryTimestampResultField = document.getElementById(
+  'diary-result-timestamp'
+);
 
 // DB 가져오기
 var database = firebase.database();
@@ -42,47 +45,57 @@ function guid() {
 }
 
 // 개발 일지 읽기
-var diaryTitleRef = database.ref('diary_title');
-var diaryContentsRef = database.ref('diary_contents');
-diaryTitleRef.on('child_added', function (snapshot) {
+var diaryRef = database.ref('diary');
+diaryRef.on('child_added', function (snapshot) {
   var data = snapshot.val();
-  var diaryTitle = data.text;
+  var diaryTitle = data.title;
+  var diaryContents = data.contents;
+  var diaryTimestamp = data.timestamp;
   if (diaryTitle != undefined) {
-    diaryTitleResult.value += diaryTitle + '\n';
+    diaryTitleResultField.innerHTML += diaryTitle + '\n';
   }
-});
-diaryContentsRef.on('child_added', function (snapshot) {
-  var data = snapshot.val();
-  var diaryTitle = data.text;
-  if (diaryTitle != undefined) {
-    diaryTitleResult.value += diaryTitle + '\n';
+
+  if (diaryContents != undefined) {
+    diaryContentsResultField.innerHTML += diaryContents + '\n';
+  }
+
+  if (diaryTimestamp != undefined) {
+    diaryTimestampResultField.innerHTML += diaryTimestamp + '\n';
   }
 });
 
 // 전부 삭제되면 결과창의 내용도 지워 준다.
-var diaryTitleRef = database.ref('diary_title');
-diaryTitleRef.on('child_removed', function (snapshot) {
-  diaryTitleResult.value = '';
-  diaryTitleField.value = '';
+diaryRef.on('child_removed', function (snapshot) {
+  diaryTitleResultField.innerHTML = '';
+  diaryContentsResultField.innerHTML = '';
+  diaryTimestampResultField.innerHTML = '';
 });
 
 // 메세지 저장
 function savedata() {
   var uuid = guid();
   var diaryTitle = diaryTitleField.value;
+  var diaryContents = diaryContentsField.value;
+  var diaryTimestamp = Date.now();
   if (diaryTitle == '') {
     alert('제목을 입력하세요');
     return true;
+  } else if (diaryContents == '') {
+    alert('내용을 입력하세요');
+    return true;
   }
-  database.ref('title/' + uuid).set({
-    text: diaryTitle,
+  database.ref('diary/' + uuid).set({
+    title: diaryTitle,
+    contents: diaryContents,
+    timestamp: diaryTimestamp,
   });
   diaryTitleField.value = '';
+  diaryContentsField.value = '';
 }
 
 // 삭제
 function deleteall() {
-  database.ref('title').remove();
+  database.ref('diary').remove();
 }
 
 // 해리포터와 마법사의 돌  1, 2
