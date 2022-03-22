2432a0e2cc7de0ce8c7bae1e740960242f49b09a
leejaeseong11 <easeon78@gmail.com>
2022-03-23 00:48:13 +0900

    Hide diary contents signly

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 var masterPassword = '1';
+
 // Initialize Firebase
 const config = {
   apiKey: 'AIzaSyBwzEE5THOJ6RdHkxu-82_8Eg4xqcZEYFE',
@@ -11,8 +12,7 @@ const config = {
 };
 firebase.initializeApp(config);
 
-// // Firebase 초기화
-// firebase.initializeApp(firebaseConfig);
+// Firebase 초기화
 var diaryTitleField = document.getElementById('diary-form-title');
 var diaryContentsField = document.getElementById('diary-form-contents');
 var diaryTitleResultField = document.getElementById('diary-result-title');
@@ -61,32 +61,27 @@ diaryRef.on('child_added', function (snapshot) {
   var diaryTitle = data.title;
   var diaryContents = data.contents;
   var diaryTimestamp = data.timestamp;
-
+  var diaryKey = snapshot.key;
   var newDiaryTitle = document.createElement('p');
-  newDiaryTitle.setAttribute('class', 'pTag');
+  newDiaryTitle.setAttribute('class', 'diary-result-contents-one');
+  newDiaryTitle.setAttribute('id', diaryKey);
   newDiaryTitle.innerHTML = diaryTitle;
+  newDiaryTitle.addEventListener('click', () => {
+    var diaryResultContentOne = document.getElementById('contents-' + diaryKey);
+
+    diaryResultContentOne.style.display =
+      diaryResultContentOne.style.display == 'none' ? 'block' : 'none';
+  });
   diaryTitleResultField.appendChild(newDiaryTitle);
 
   var newDiaryTimestamp = document.createElement('p');
-  newDiaryTimestamp.setAttribute('class', 'pTag');
   newDiaryTimestamp.innerHTML = diaryTimestamp;
   diaryTimestampResultField.appendChild(newDiaryTimestamp);
 
   var newDiaryContents = document.createElement('p');
-  newDiaryContents.setAttribute('class', 'pTag');
+  newDiaryContents.setAttribute('id', 'contents-' + diaryKey);
   newDiaryContents.innerHTML = diaryContents;
   diaryContentsResultField.appendChild(newDiaryContents);
-  // if (diaryTitle != undefined) {
-  //   diaryTitleResultField.innerHTML += diaryTitle + '\n';
-  // }
-
-  // if (diaryContents != undefined) {
-  //   diaryContentsResultField.innerHTML += diaryContents + '\n';
-  // }
-
-  // if (diaryTimestamp != undefined) {
-  //   diaryTimestampResultField.innerHTML += diaryTimestamp + '\n';
-  // }
 });
 
 // 메세지 저장
@@ -157,5 +152,6 @@ function onClose() {
 var diaryDeleteConfirm = document.getElementById('diary-delete-confirm');
 var diaryDeletePasswrod = document.getElementById('diary-delete-password');
 diaryDeleteConfirm.addEventListener('click', onClose);
+
 // 해리포터와 마법사의 돌  1, 2
 // 덤블도어의 수련회 메타에 타락할 수 밖에 없었던 슬리데린..
--- src/index.js
+++ src/index.js
@@ -5,12 +5,3 @@ function openForm() {
 function closeForm() {
   document.getElementById('diary-form-popup').style.display = 'none';
 }
-
-// 개발 일지 제목 클릭 시 상세 내용 출력
-var diaryResultTitle = document.getElementById('diary-result-title');
-diaryResultTitle.addEventListener('click', () => {
-  var diaryResultContents = document.getElementById('diary-result-contents');
-
-  diaryResultContents.style.display =
-    diaryResultContents.style.display == 'none' ? 'block' : 'none';
-});
