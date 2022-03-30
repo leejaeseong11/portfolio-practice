44cbc2d03e0295b21e131d9ec59a6ee08edbf469
leejaeseong11 <easeon78@gmail.com>
2022-03-31 00:54:46 +0900

    Add book review form

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ function openDiaryForm() {
 function closeDiaryForm() {
   document.getElementById('diary-form-popup').style.display = 'none';
 }
+
+function openBookForm() {
+  document.getElementById('book-form-popup').style.display = 'block';
+}
+
+function closeBookForm() {
+  document.getElementById('book-form-popup').style.display = 'none';
+}
