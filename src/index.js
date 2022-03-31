67df8c7e5fedaedf38ee3ccb51e8362bb4c64ae9
leejaeseong11 <easeon78@gmail.com>
2022-04-01 02:52:26 +0900

    Add book image upload

--- src/index.js
+++ src/index.js
@@ -13,3 +13,14 @@ function openBookForm() {
 function closeBookForm() {
   document.getElementById('book-form-popup').style.display = 'none';
 }
+
+function uploadChange(file) {
+  var el = file.parentNode.parentNode.getElementsByTagName('*');
+  for (var i = 0; i < el.length; i++) {
+    var node = el[i];
+    if (node.className == 'file-text') {
+      node.innerHTML = file.value;
+      break;
+    }
+  }
+}
