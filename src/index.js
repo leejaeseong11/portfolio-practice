86896e05adfb9485e2883012029d35b282f9ccbe
leejaeseong11 <easeon78@gmail.com>
2022-04-20 01:26:34 +0900

    Fix project image hover explanation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+// 이미지 슬라이드 기능
+var slideWrapper = document.querySelector('.container');
+var slides = document.querySelectorAll('.item');
+var totalSlides = slides.length;
+
+var sliderWidth = slideWrapper.clientWidth;
+var slideIndex = 0;
+var slider = document.querySelector('.slider');
+
+slider.style.width = sliderWidth * totalSlides + 'px';
+
+showSlides();
+
+function showSlides() {
+  for (var i = 0; i < slides.length; i++) {
+    slider.style.left = -(sliderWidth * slideIndex) + 'px';
+  }
+  slideIndex++;
+  if (slideIndex === totalSlides) {
+    slideIndex = 0;
+  }
+  setTimeout(showSlides, 3000);
+}
+
+// 프로젝트 이미지에 mouseover 시 프로젝트 설명 나타남
+var portfolioContentsImg = document.querySelectorAll('.portfolio-contents-img');
+
+for (var i = 0; i < portfolioContentsImg.length; i++) {
+  var portfolioContentsExplanation = document.querySelectorAll(
+    '.portfolio-contents-explanation'
+  )[i];
+  portfolioContentsImg[i].addEventListener('mouseover', (e) => {
+    e.target.nextSibling.nextSibling.style.display = 'block';
+  });
+  portfolioContentsImg[i].addEventListener('mouseout', (e) => {
+    e.target.nextSibling.nextSibling.style.display = 'none';
+  });
+}
+
+// todo
 function openDiaryForm() {
   document.getElementById('diary-form-popup').style.display = 'block';
 }
@@ -38,42 +78,4 @@ function openMenu() {
 
 function closeMenu() {
   menuBar.style.display = 'none';
-}
-
-// 이미지 슬라이드 기능
-var slideWrapper = document.querySelector('.container');
-var slides = document.querySelectorAll('.item');
-var totalSlides = slides.length; // item의 갯수
-
-var sliderWidth = slideWrapper.clientWidth; // container의 width
-var slideIndex = 0;
-var slider = document.querySelector('.slider');
-
-slider.style.width = sliderWidth * totalSlides + 'px';
-
-showSlides();
-
-function showSlides() {
-  for (var i = 0; i < slides.length; i++) {
-    slider.style.left = -(sliderWidth * slideIndex) + 'px';
-  }
-  slideIndex++;
-  if (slideIndex === totalSlides) {
-    slideIndex = 0;
-  }
-  setTimeout(showSlides, 3000);
-}
-
-// 프로젝트 mouseover 시 프로젝트 설명 나타남
-var portfolioContentsImg = document.querySelectorAll('.portfolio-contents-img');
-
-for (var i = 0; i < portfolioContentsImg.length; i++) {
-  var portfolioContentsExplanation = document.querySelectorAll(
-    '.portfolio-contents-explanation'
-  )[i];
-  portfolioContentsImg[i].addEventListener('mouseover', () => {
-    console.log(this.parentNode);
-    portfolioContentsExplanation.display = 'block';
-    console.log('TEST');
-  });
 }
